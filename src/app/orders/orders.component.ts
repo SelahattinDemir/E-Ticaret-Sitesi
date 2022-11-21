a1bc98039fb5450d2d963e58633436ff391d670b
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-21 22:25:16 +0300

    orders page created

---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../Services/api.service';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+  
+  public orders:any=[]
+
+  constructor(private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.api.ordersApi().subscribe(
+      res=>{
+      this.orders=res  
+      }
+    )
+  }
+
+}

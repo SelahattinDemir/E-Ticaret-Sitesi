4b0acf6c42776ffb2ae6058d1c53bb1f0a9eb52b
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-21 22:49:46 +0300

    newly added components are imported into app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { BasketComponent } from './basket/basket.component';
+import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductsdetailComponent } from './productsdetail/productsdetail.component';
+import { AuthGuard } from './Services/auth.guard.service';
+import { UnsavedGuard } from './Services/unsaved.guard.services';
+import { SignupComponent } from './signup/signup.component';
+
 //page routes for components
 const routes: Routes = [
-  {path:'products', component: ProductsComponent},
-  {path:'products/:id', component: ProductsdetailComponent}
+  {path:'products', component: ProductsComponent, canActivate:[AuthGuard]},
+  {path:'products/:id', component: ProductsdetailComponent, canActivate:[AuthGuard]},
+  {path:'basket', component: BasketComponent, canActivate:[AuthGuard]},
+  {path:'orders', component: OrdersComponent, canActivate:[AuthGuard]},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'dashboard',component:AdminDashboardComponent},
   //Wildcard invalid routes and guard will be redirected to here 
+  {path:'**', redirectTo:'login',pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,57 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProductsComponent } from './products/products.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsdetailComponent } from './productsdetail/productsdetail.component';
 import { ApiService } from './Services/api.service';
-import { CartService } from './Services/card.services';
-import { AuthGuard } from './Services/auth.guard';
+import { CartService } from './Services/cart.services';
+import { AuthGuard } from './Services/auth.guard.service';
+import { FilterHandlerPipe } from './filtered/filter-handler.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BasketComponent } from './basket/basket.component';
+import { OrdersComponent } from './orders/orders.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ErrorHandleInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     HeaderComponent,
-    ProductsdetailComponent
+    ProductsdetailComponent,
+    FilterHandlerPipe,
+    BasketComponent,
+    OrdersComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [ApiService, CartService],
+  providers: [ApiService,
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi:true
+    },
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: ErrorHandleInterceptor, 
+      multi: true 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

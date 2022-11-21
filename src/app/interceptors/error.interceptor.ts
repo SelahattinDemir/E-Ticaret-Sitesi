798841b27b476e1e3fe2af0db463b441f3599feb
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-21 22:46:50 +0300

    Added interceptor for HTTP requests errors

---
+++ src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,35 @@
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+export class ErrorHandleInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return this.handleError(error);
+      })
+    )
+  }
+  //Client Errors by result for HTTP requests
+  handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'Bilinmeyen mesaj';
+
+    switch (errorRes.error.code) {
+      case 400:
+        errorMessage = 'Geçersiz istek';
+        break;
+      case 401:
+        errorMessage = 'Yetkisiz';
+        break;
+      case 403:
+        errorMessage = 'Yasaklı';
+        break;
+      case 404:
+        errorMessage = 'Bulunamadı';
+        break;
+    }
+    alert(errorMessage);
+
+    return throwError(errorMessage);
+  }
+
+}

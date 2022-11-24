ef33ae476b0a6b9687e95ad18e3e1f915b2026ee
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-24 14:55:08 +0300

    Updated navbar display based on user's login status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../Services/api.service';
-import { CartService } from '../Services/cart.services';
+import { CartService } from '../Services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -10,16 +10,15 @@ import { CartService } from '../Services/cart.services';
 export class HeaderComponent implements OnInit {
 
   public totalItem : number = 0;
-  public searchTerm !: string;
-  public isLoggedIn:boolean=false;
+  public searchTerm: string = "";
+  public user:any;
+
   constructor(private cartService : CartService, private apiservice:ApiService) { }
-
-
 
   ngOnInit(): void {
     // checking user for logout template
-    this.apiservice.checkUser.subscribe((res) => {
-      this.isLoggedIn = res;
+    this.apiservice.user.subscribe((res) => {
+      this.user = res;
     })
     //taking data for template of total cart item number
     this.cartService.getProducts()
@@ -31,17 +30,13 @@ export class HeaderComponent implements OnInit {
   //checking ınput to search
   search(event:any){
     this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
     this.cartService.search.next(this.searchTerm);
   }
 
   // clearing local storage for guard and isloggedin for logout button
-
   logout(){
-    this.apiservice.logout()
-    this.apiservice.checkUser.subscribe((res) => {
-    this.isLoggedIn = res;
-    })
+    this.apiservice.user.next("")
+    localStorage.clear()
   }
-
-
 }

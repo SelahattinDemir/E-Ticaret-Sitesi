4f69a5a3086673e4d524a659bdbaf37408565794
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-24 14:53:35 +0300

    quantity settings have been made, checkout settings adjusted and added random order number.

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CartService } from '../Services/cart.services';
+import { CartService } from '../Services/cart.service';
 import { ApiService } from '../Services/api.service';
 import { Router } from '@angular/router';
 
@@ -11,28 +11,41 @@ import { Router } from '@angular/router';
 export class BasketComponent implements OnInit {
 
   public cartItems: any = [];
-  public grandTotal !: number;
+  public grandTotal: any = 0;
 
   constructor(private cartService: CartService, private api:ApiService, private router: Router,) { }
-
-
 
   ngOnInit(): void {
     this.cartService.getProducts()
       .subscribe(res => {
         this.cartItems = res;
-        this.grandTotal = this.cartService.getTotalPrice();
+        this.cartItems.forEach((item: any) => {
+          Object.assign(item, { quantity: 1, total: Number(item.price) });
+          //commentler sipariş ekranına gitmesin diye 
+          delete item.comment;
+        });
+        for(let item of this.cartItems){
+          this.grandTotal += Number(item.total);
+        }
+        console.log(this.cartItems);
       });
+  }
+
+  quantity(quantity:any, index:number){
+    this.cartItems[index].quantity = Number(quantity.target.value);
+    this.cartItems[index].total = Number(this.cartItems[index].price*quantity.target.value);
+    console.log(this.cartItems[index].total);
   }
 
   removeItem(item: any) {
     this.cartService.removeCartItem(item);
   }
-
-
-
+  //add to the cart ı cağırıp item in adından 1 tane daha karta ekleme yapılabilir.ama sayaç nasıl çalıaşacak belli değil
   checkout(){
-    this.api.checkoutApi(this.cartItems).subscribe();
+    let user:any = this.api.getToken();
+    let orderNumber = Math.floor(Math.random()*1000000);
+    let order:any = [{"user":user, "ordernumber":orderNumber}, {"order":this.cartItems}];
+    this.api.checkoutApi(order).subscribe();
     this.cartService.removeAllCart();
     this.router.navigate(["orders"]);
   }

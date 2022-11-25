064676dbca876ba528b24134719b0b410227ee5c
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 23:56:35 +0300

    Changed the comment attribute to comments and Reactive form structure created for comments

--- src/app/productsdetail/productsdetail.component.ts
+++ src/app/productsdetail/productsdetail.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
@@ -18,25 +20,43 @@ export class ProductsdetailComponent implements OnInit, OnDestroy {
   //public bc they will reached by template
   public productsDetail: any;
   public currentUrl!: string;
+  public commentForm!: FormGroup;
+  public rating = 3;
+  private itemId: any;
 
-  constructor(private route: ActivatedRoute, private api: ApiService, private cartService: CartService, private toastr: ToastrService) { }
+  constructor(private route: ActivatedRoute, private api: ApiService, private cartService: CartService, private toastr: ToastrService, private formBuilder:  FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
     //With paramMap you can route multiple page with only one template 
-    let itemId: any = this.route.snapshot.paramMap.get("id");
-    this.currentUrl = `?id=${Number(itemId)}`;
+    this.itemId = Number(this.route.snapshot.paramMap.get("id"));
+    this.currentUrl =`?id=${this.itemId}`;
     //For template view taking data from server 
-    //CURRENT ID API YE YOLLA GELENİ KAYDET SADECE 
     this.sub$ = this.api.filterApi(this.currentUrl).subscribe(
       res => {
         this.productsDetail = res;
     })
+    this.commentForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      comment: ["", Validators.required]
+    })
   }
 
+  //adding to cart from product detail page
   addtocart(item: any){
-    //adding to cart from product detail page
     this.cartService.addtoCart(item);
     this.toastr.success('Ürün Sepete Eklendi', 'Checkout');
+  }
+
+  submit(){
+    let date = new Date().toString();
+    let comment = {"username": this.commentForm.value.username, "comment": this.commentForm.value.comment};
+    Object.assign(comment, {"time": date})
+    // STAR service yazılacak EKLENECEK 
+    this.productsDetail[0].comments.push(comment);
+    this.api.updateItemApi(this.productsDetail[0], this.itemId).subscribe(res => {
+    this.commentForm.reset();
+    this.toastr.success('Ürün Eklendi', 'Success');;
+  })
   }
 
   ngOnDestroy(): void {

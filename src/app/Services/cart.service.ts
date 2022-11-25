3f682fb765b9d54f4e7dc2a47293bfeb6c3824c5
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 21:58:56 +0300

    Added product model and comments. Edited in functions

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,56 +1,56 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+import { product } from '../models/product';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-// Values for outside communication
-//Bey Behavior subject you can give initial value , you can next another observable anytime , you can emmit data and observe it
+  // Values to communication between components
+  //Behavior subject you can give initial value , you can next another observable anytime inside, you can emmit data and observe it
 
-  public cartItemList : any =[]
-  private productList = new BehaviorSubject<any>([]);
-  public search = new BehaviorSubject<any>("");
+  public cartItemList: product[] = [];
+  // productlist main data storage for service it is private so you can not reach directly
+  private productList = new BehaviorSubject<product[]>([]);
+  //Carrying data between navbar and products components for filtering pipe operation. 
+  public search = new BehaviorSubject<string>("");
 
   constructor() { }
 
+  //methods for safety to reach productlist from outside
   getProducts(){
-    // asobservable güvenlik sağlamak amaçlı kimse bu getproduct'a ulaşanlar next yapamasın diye 
-    //BÜTÜN SİSTEM PRODUCTLİST ÜZERİNDE Çalışıyor cart item list aracı 
     return this.productList.asObservable();
   }
 
   getSearch() { 
     return this.search.asObservable();
- }
+  }
 
-  setProduct(product : any){
+  setProduct(product: product[]){
     this.cartItemList.push(...product);
     this.productList.next(product);
   }
-  addtoCart(product : any){
+
+  //Adding products to cart from products page
+  addtoCart(product: product){
     this.cartItemList.push(product);
     this.productList.next(this.cartItemList);
-    this.getTotalPrice();
-    console.log(this.cartItemList)
   }
-  getTotalPrice() : number{
-    let grandTotal = 0;
-    this.cartItemList.map((price:any)=>{
-      grandTotal += price.total;
-    })
-    return grandTotal;
-  }
-  removeCartItem(product: any){
-    this.cartItemList.map((item:any, index:any)=>{
-      if(product.id=== item.id){
-        this.cartItemList.splice(index,1);
+
+  //Removing item from cart
+  removeCartItem(product: product){
+    this.cartItemList.map((item: any, index: number)=>{
+      if(product.id === item.id){
+        this.cartItemList.splice(index, 1);
       }
     })
     this.productList.next(this.cartItemList);
   }
+
+  // Removing all cart for checkout 
   removeAllCart(){
-    this.cartItemList = []
+    this.cartItemList = [];
     this.productList.next(this.cartItemList);
   }
 }

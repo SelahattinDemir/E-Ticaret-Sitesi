10fea5afd3da713c9b8ffb39ab0408090c336910
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 11:52:44 +0300

    Added alert using Toastr

--- src/app/Services/auth.guard.service.ts
+++ src/app/Services/auth.guard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,16 +8,16 @@ import { CanActivate, Router, UrlTree } from '@angular/router';
 //Guards should be supported with back end this way is not legit way
 export class AuthGuard implements CanActivate {
 
-  constructor(private router:Router){}
+  constructor(private router: Router, private toastr: ToastrService){}
 
   canActivate(): boolean {
 //If anyone logged in registered in local storage 
 //Also if they logout local storage will be deleted and this will return false
     if(localStorage.getItem('token')){
-       return true;
+      return true;
     }
     else{     
-      alert("Giriş yapmadan erişemezsiniz");
+      this.toastr.error('Giriş yapmadan erişemezsiniz');
       this.router.navigate(['']);
       return false;
     }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,38 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 import { ApiService } from '../Services/api.service';
+import { passwordMatchValidatorService } from '../Services/passwordMatchValidator.service';
+import { usernameValidator } from '../Services/regexValidator.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
-  signupForm!:FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private router:Router, private api:ApiService) { }
+export class SignupComponent implements OnInit {
+
+  signupForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private api: ApiService, private match: passwordMatchValidatorService, private toastr: ToastrService) { }
+
+  //using pattern att. for regex valid.(common patterns are used)
+  emailPattern =/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  pwdPattern =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
   ngOnInit(): void {
-    this.signupForm=this.formBuilder.group({
-      // validators for signup form
-    username:["", Validators.required],
-    email:["", [Validators.required, Validators.email]],
-    password:["",[Validators.required,Validators.minLength(6)]],
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    this.signupForm = this.formBuilder.group({
 
-    })
+    //3 Type of Validation Added
+    // usernameValidator added in default validation from regexValidator.ts (1)
+    username:["", usernameValidator],
+    // default validators(2)
+    email:["", Validators.pattern(this.emailPattern)],
+    password:["", [Validators.required, Validators.pattern(this.pwdPattern)]],
+    confirmpassword:[""],
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+    role:"user"
+    },
+    {
+      //custom validator(3)from passwordMatchValidator.ts
+      validator:this.match.passwordMatchValidator('password','confirmpassword')
+    }
+    )
   }
 
   // sending data with api.signUp 
 
   signUp(){
     this.api.signUp(this.signupForm).subscribe(res=>{
-      alert("You have signed up successfully ")
-      this.signupForm.reset()
-      this.router.navigate(["login"]) 
-  },err=>{
-    alert("Something went wrong")
-  })
+      this.toastr.success('Başarıyla kaydoldunuz');
+      this.signupForm.reset();
+      this.router.navigate(["login"]);
+    },err=>{
+      this.toastr.success('Bir şeyler yanlış gitti');
+    })
   }
 }

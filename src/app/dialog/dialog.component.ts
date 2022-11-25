fab8352d68a2a6879e85ce37eeec199013fd085d
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 21:57:13 +0300

    Added product model and comments

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -4,6 +4,7 @@ import { ToastrService } from 'ngx-toastr';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { ApiService } from '../Services/api.service';
+import { product } from '../models/product';
 
 @Component({
   selector: 'app-dialog',
@@ -12,14 +13,17 @@ import { ApiService } from '../Services/api.service';
 })
 export class DialogComponent implements OnInit {
 
-  formValue!: FormGroup;
-  item: any = {};
-  itemData: any;
-  actionButton: string = "Ekle";
-  
-  constructor(private api: ApiService, private formBuilder: FormBuilder, private toastr: ToastrService, private dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public row:any) { }
+  public formValue !: FormGroup;
+  //for showing update or add button depends on situation
+  public actionButton: string = "Ekle";
 
+  //Angular dialog metarial working like modal but more complex and efficent
+  //Dashboard dialog actions handling with mat-dialog-ref
+  //Data transfered with mat-dialog-data taking data from table(dashboard component) 
+  constructor(private api: ApiService, private formBuilder: FormBuilder, private toastr: ToastrService, private dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public row: product) { }
+    
+  //validators for dialog form
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
       title: ['', Validators.required],
@@ -32,6 +36,7 @@ export class DialogComponent implements OnInit {
       comment: ['']
     })
 
+    //setting formvalues for updating action
     if(this.row){
       this.actionButton = "Güncelle"
        this.formValue.controls['title'].setValue(this.row.title)
@@ -40,45 +45,55 @@ export class DialogComponent implements OnInit {
        this.formValue.controls['description'].setValue(this.row.description)
        this.formValue.controls['category'].setValue(this.row.category)
        this.formValue.controls['image'].setValue(this.row.image)
+       this.formValue.controls['star'].setValue(this.row.star)
+       this.formValue.controls['comment'].setValue(this.row.comment)
     }
   }
 
+  // if you click update return update action else posting item to db with form value
   postItem() {
     if(!this.row){
-     if(this.formValue.value){
-       this.api.postItemApi(this.formValue.value)
-       .subscribe(res => {
-         this.formValue.reset();
-         this.dialogRef.close();
-         this.toastr.success('Ürün Eklendi', 'Success');
-       })
+      if(this.formValue.value){
+        this.api.postItemApi(this.formValue.value)
+        .subscribe(res => {
+          this.formValue.reset();
+          this.toastr.success('Ürün Eklendi', 'Success');
+        })
       }
-     else{
-       this.toastr.error('Form Değerleri Geçerli Değil', 'Error');
-       this.dialogRef.close(this.formValue);
-     }
+      else{
+        this.toastr.error('Form Değerleri Geçerli Değil', 'Error');
+        //sending form data with close action so dashboard and guard can use 
+        this.dialogRef.close(this.formValue);
+      }
     }
     else{
       this.updateItem();
     }
-   }
+  }
    
-   updateItem(){
-     if(this.formValue.valid){
-       this.api.updateItemApi(this.formValue.value, this.row.id)
-       .subscribe(res => {
-         this.formValue.reset();
-         this.dialogRef.close();
-         this.toastr.success('Ürün Eklendi', 'Success');
-       })
-      }
-     else{
-       this.toastr.error('Form Değerleri Geçerli Değil', 'Error');
-       this.dialogRef.close(this.formValue);
-     }
-   }
-   close(){
-     this.dialogRef.close(this.formValue);
-   }
+   //updating item to db with form value and row.id(which they picked with button)
+  updateItem(){
+    if(this.formValue.valid){
+      this.api.updateItemApi(this.formValue.value, this.row.id)
+      .subscribe(res => {
+        this.formValue.reset();
+        this.toastr.success('Ürün Eklendi', 'Success');
+      })
+    }
+    else{
+      this.toastr.error('Form Değerleri Geçerli Değil', 'Error');
+      //sending form data with close action so dashboard and guard can use 
+      this.dialogRef.close(this.formValue);
+    }
+  }
+  close(){
+    //sending form data with close action so dashboard and guard can use 
+    this.dialogRef.close(this.formValue);
+  }
+
+  //Error handling for formvalidation  at template 
+  public errorHandling = (control: string, error: string) => {
+    return this.formValue.controls[control].hasError(error);
+  }
 
 }

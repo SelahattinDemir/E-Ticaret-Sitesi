6a4ff99880f1cfefa453659a3f8ead4d74c59601
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 22:06:15 +0300

    imported product filters service, added comments, edited in functions and made more readable

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 import { ApiService } from '../Services/api.service';
 import { CartService } from '../Services/cart.service';
+import { ProductfiltersService } from '../Services/productfilters.service';
 
 @Component({
   selector: 'app-products',
@@ -13,29 +13,31 @@ import { CartService } from '../Services/cart.service';
 })
 
 export class ProductsComponent implements OnInit {
-   //TEMPLATE DEN ERİŞMEK İÇİN PUBLİC OLACAK
-  public star: number[] = [1, 2, 3, 4, 5];
-
-  public products : any;
-  public filteredProducts : any;
+   
+  private products: any;
+  //var. for templates
+  public star: number[] = [1, 2, 3, 4, 5]; 
+  public filteredProducts: any;
   public searchKey: string = "";
   public viewGrid = true;
-  public uniqueBrands:any=[];
+  public uniqueBrands: any = [];
+  public filteringURL = `?`;
 
-  constructor(public fb: FormBuilder, private api: ApiService, private cartService: CartService, private router: Router, private toastr: ToastrService) { }
+  constructor(private api: ApiService, private cartService: CartService, private router: Router, private toastr: ToastrService, private productFilters: ProductfiltersService) { }
 
   ngOnInit(): void {
     //getting product from server
     this.api.getAllProductApi()
-    .subscribe(res=>{
+    .subscribe(res => {
       this.products = res;
       this.filteredProducts = res;
-      //unique brandleri bulma burada yapıldı serverdan gelen değişimlerde direk olarak tepki vermesi amaçlı yani dinamik
-      this.products.forEach((element:any) => {
+      //checking unique brands for same brand inputs from db 
+      //so can display dynamically in brand filter
+      this.products.forEach((element: any) => {
         return this.uniqueBrands.push(element.brand);
       });
-      this.uniqueBrands = this.uniqueBrands.filter((item:any, 
-        index:any) => this.uniqueBrands.indexOf(item) === index);
+      this.uniqueBrands = this.uniqueBrands.filter((item: any, 
+        index: any) => this.uniqueBrands.indexOf(item) === index);
     });
     // data transfer for search handling ( filter in subcribe not working so pipe is used)
     this.cartService.getSearch().subscribe((val: any) => {
@@ -43,25 +45,18 @@ export class ProductsComponent implements OnInit {
     })
   }
 
+  // Sending element id to product detail paramMap
+  onSelect(item: any) {
+    this.router.navigate(["/products", item.id])
+  }
+
+  // adding item to cart
   addtocart(item: any){
     this.cartService.addtoCart(item);
     this.toastr.success('Ürün Sepete Eklendi ', 'Checkout');
   }
 
-  //category filtering 
-   // APİ isteği ile yapılabilir. ama hali hazırda datayı aldım neden bir daha istek atayım
-  //SERVİS YAZILABİLİR AMA BAŞKA YERDE LAZIM DEĞİL ŞİMDİLİK GEREK YOK
-  // BÜTÜN FİLTRELER ORGANİZE ÇALIŞMIYOR AYRI BİR DEĞİŞKEN DAHA TANIMLAMA GEREKBİLİR
-
-  categoryFilterHandler(category: string){
-    this.filteredProducts = this.products
-    .filter((product:any) => {
-      if(product.category == category || category == ''){
-        return product;
-      }
-    })
-  }
-
+  //changing view style
   viewHandler(category: string) {
     if(category === "grid") {
       this.viewGrid = true;
@@ -71,90 +66,24 @@ export class ProductsComponent implements OnInit {
     }
   }
 
-  //REFACTOR SWTİCH CASE
+  //Filtering Operations(also can be done by api requests)
+  categoryFilter(category: string) {
+    this.productFilters.categoryFilterService(category)
+    this.productFilters.data.subscribe(res => this.filteredProducts = res)
+  }
+
   priceFilter(min: any, max: any) {
-    this.filteredProducts = this.products
-      .filter((product: any) => {
-        if(max !== "") {
-          if(Number(min) <= Number(product.price)) {
-            return product;
-          }
-        }
-        if(min === "") {
-          if(Number(product.price <= Number(max))) {
-            return product;
-          }
-        }
-        else {
-          if(Number(min) <= Number(product.price) && Number(product.price) <= Number(max)) {
-            return product;
-          }
-        }
-      })
+    this.productFilters.priceFilterService(min, max)
+    this.productFilters.data.subscribe(res => this.filteredProducts = res)
   }
 
-  // Sending element id to product detail paramMap
-  onSelect(item:any){
-    this.router.navigate(["/products",item.id])
+  brandFilter(brand: any) { 
+    this.productFilters.brandFilterService(brand)
+    this.productFilters.data.subscribe(res => this.filteredProducts = res)
   }
 
-  // DB den gelen brand e göre şekil alıyor yani dinamik
-  selectedBrands: any = [];
-  tempBrands: any = [];
-  brandFilter(brand: any) {
-    if(brand.target.checked) {
-      this.tempBrands.push(brand.target.value);
-      this.selectedBrands = [];
-      for(let i = 0; i < this.tempBrands.length; i++) {
-        this.selectedBrands.push(this.products.filter((element: any) => element.brand == this.tempBrands[i]));
-      }
-    }
-    else {
-      this.tempBrands = this.tempBrands.filter((element: any) => element != brand.target.value);
-      this.selectedBrands = [];
-      for(let i = 0; i < this.tempBrands.length; i++) {
-        this.selectedBrands.push(this.filteredProducts.filter((element: any) => element.brand == this.tempBrands[i]));
-      }
-      this.filteredProducts = this.selectedBrands;
-      console.log(this.selectedBrands);
-    }
-    this.filteredProducts = {};
-    //flat() dışardaki [] siliyor bu sayede 3 object e dönüyor [0] işe yaramıyor.
-    this.filteredProducts = this.selectedBrands.flat();
-    console.log(this.filteredProducts);
-    if(this.selectedBrands.length === 0) {
-      this.filteredProducts = this.products;
-    }
-  }
-  starFilter(star:any){
-    if(star.target.value == 1)
-    this.filteredProducts = this.products
-    .filter((product: any) => {
-      if (0 <= product.star && product.star <=5) {
-        return product;
-      }
-    })
-    if(star.target.value == 2)
-    this.filteredProducts = this.products
-    .filter((product: any) => {
-      if (4 <= product.star ) {
-        return product;
-      }
-    })
-    if(star.target.value == 3)
-    this.filteredProducts = this.products
-    .filter((product: any) => {
-      if (3 <= product.star && product.star < 4 ) {
-        return product;
-      }
-    })
-    if(star.target.value == 4)
-    this.filteredProducts = this.products
-    .filter((product: any) => {
-      if (0 <= product.star && product.star < 3 ) {
-        return product;
-      }
-    })
-
+  starFilter(star: any){
+    this.productFilters.starFilterService(star)
+    this.productFilters.data.subscribe(res => this.filteredProducts = res)
   }
 }

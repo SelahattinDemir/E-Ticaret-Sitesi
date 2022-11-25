f0e30c166d964dd86c56fa4642237d0a3b1196b8
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 11:53:50 +0300

    Added alert using Toastr

--- src/app/Services/admin.guard.service.ts
+++ src/app/Services/admin.guard.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 import { ApiService } from './api.service';
 
 @Injectable({
@@ -7,20 +9,18 @@ import { ApiService } from './api.service';
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private api:ApiService, private router:Router){}
+  constructor(private api: ApiService, private router: Router, private toastr: ToastrService){}
 
-  canActivate():boolean{
-    const user=this.api.user.value
-        if(user.role == "admin"){
-
-          return true
-        } 
-        else{
-          alert("İzinsiz giremezsiniz")
-          this.router.navigate([""])
-          localStorage.clear()
-          return false
-        }
+  canActivate(): boolean {
+    const user = this.api.user.value;
+      if(user.role == "admin"){
+        return true;
+      } 
+      else{
+        this.toastr.error('İzinsiz giremezsiniz');
+        this.router.navigate([""]);
+        localStorage.clear();
+        return false;
       }
-
+    }
   }

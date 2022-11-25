de9e6e5539292511649d51432bb114b2161d3467
Selahattin Demir <selahattindemir686@gmail.com>
2022-11-25 11:44:46 +0300

    Rewritten with angular material

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+
 import { ApiService } from '../Services/api.service';
+import { DialogComponent } from '../dialog/dialog.component';
+import { ConfirmDialogComponent, ConfirmDialogModel } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -9,110 +16,82 @@ import { ApiService } from '../Services/api.service';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  formValue !: FormGroup;
-  adminData : any;
-  adminObj : any={}
-  showAdd !: boolean;
-  showUpdate !: boolean;
-  receive !: string;
-  categorySpecification !: any;
+  displayedColumns: string[] = ['id', 'title', 'brand', 'price','description','category','image','star','action'];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private api: ApiService, private formBuilder: FormBuilder) { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  public data: any;
+  dialogformvalue: any = {};
+
+  constructor(public dialog: MatDialog, private api:ApiService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.formValue = this.formBuilder.group({
-    title: ['',Validators.required],
-    price: ['',Validators.required],
-    description: ['',Validators.required],
-    category: ['',Validators.required],
-    image: ['',Validators.required],
-    brand: ['',Validators.required],
-    star:[0],
-    comment:[]
-    })
-    this.gettingData()
+    this.getItem();
   }
 
-  gettingData(){
-    this.api.Getadmins()
-    .subscribe(res=>{
-      this.adminData = res
-    })
+  openDialog() {
+    this.dialog.open(DialogComponent, {width:"50%"}).afterClosed().subscribe((resp:string)=>{
+      this.dialogformvalue = resp;
+      console.log(this.dialogformvalue);
+      this.getItem();
+     })
   }
 
-  addProduct(){
-    this.formValue.reset();
-    this.showAdd = true;
-    this.showUpdate = false;
-  }
-
-  postadminDetails() {
-    this.adminObj.title = this.formValue.value.title;
-    this.adminObj.price = this.formValue.value.price;
-    this.adminObj.description = this.formValue.value.description;
-    this.adminObj.category = this.formValue.value.category;
-    this.adminObj.image = this.formValue.value.image;
-    this.adminObj.brand = this.formValue.value.brand;
-    this.adminObj.star = this.formValue.value.star;
-    this.adminObj.comment = this.formValue.value.comment;
-    this.api.Postadmin(this.adminObj)
-    .subscribe(res => {
-      console.log(res);
-      this.gettingData()
-    })
-  }
-
-  //formdaki veriyi alıp gönderen kısım
-  editadminDetail(){
-    this.adminObj.title = this.formValue.value.title;
-    this.adminObj.price = this.formValue.value.price;
-    this.adminObj.description = this.formValue.value.description;
-    this.adminObj.category = this.formValue.value.category;
-    this.adminObj.image = this.formValue.value.image;
-    this.adminObj.brand = this.formValue.value.brand;
-    this.adminObj.star = this.formValue.value.star;
-    this.adminObj.comment = this.formValue.value.comment;
-    this.api.Updateadmin(this.adminObj)
-   .subscribe(res=>{
-     alert("Başarıyla Güncellendi")
-     let ref = document.getElementById('close');
-     ref?.click();
-     this.gettingData()
-   })
- }
-
- //edit e tıklandığında o satırdaki veriyi alıp forma veren kısım
-  onEdit(row : any){
-    this.adminObj.id = row.id;
-    this.formValue.controls['title'].setValue(row.title);
-    this.formValue.controls['price'].setValue(row.price);
-    this.formValue.controls['description'].setValue(row.description);
-    this.formValue.controls['category'].setValue(row.category);
-    this.formValue.controls['image'].setValue(row.image);
-    this.formValue.controls['brand'].setValue(row.brand);
-    this.showUpdate = true;
-    this.showAdd = false;
-  }
-
-  deleteadminDetail(row : any){
-    let clickedYes = confirm("Silmek istediğinizden emin misiniz?");
-    if(clickedYes){
-      this.api.Deleteadmin(row.id)
-      .subscribe(res=>{
-        alert("Başarıyla Silindi");
-        this.gettingData()
+  getItem(){
+    this.api.getItemApi()
+      .subscribe((res:any)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
   }
 
- canExit():boolean{
-  if(this.formValue.pristine && this.formValue.touched || this.formValue.touched && this.formValue.invalid){
-    if(confirm("Kaydedilmemiş bazı değişiklikler var")){
-      return true
-    }
-    return false
+  editItem(row:any){
+    this.dialog.open(DialogComponent, {width:'50%',data:row}).afterClosed().subscribe((resp:string)=>{
+      this.dialogformvalue = resp;
+      console.log(this.dialogformvalue);
+        this.getItem();
+    })
   }
-  return true
-}
+
+  deleteItem(id:number){
+    const message = `Bu öğeyi silmek istediğinizden emin misiniz?`;
+    const dialogData = new ConfirmDialogModel("Confirm Action", message);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: "400px",
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      if(dialogResult){
+        this.api.deleteItemApi(id).subscribe(res=>
+          this.getItem()
+        )
+      }
+    });
+  } 
+
+  canExit():boolean{
+    if(this.dialogformvalue.touched && this.dialogformvalue.pristine || this.dialogformvalue.touched && this.dialogformvalue.invalid){
+      this.toastr.error('Kaydedilmemiş Değişiklikler', 'Error')
+      if(confirm("Kaydedilmemiş bazı değişiklikler var")){
+        return true;
+      }
+      return false;
+
+    }
+    return true;
+  }
 
 }
